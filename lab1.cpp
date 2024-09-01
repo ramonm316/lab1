aab122c8360f3d96c348577d7f19ded0581dab52
ramonm316 <ramonm15267@protonmail.com>
2024-08-31 19:23:16 -0700

    lab1 homework

--- lab1.cpp
+++ lab1.cpp
@@ -9,7 +9,7 @@
 //This program needs some refactoring.
 //We will do this in class together.
 //
-//
+
 #include <iostream>
 using namespace std;
 #include <stdio.h>
@@ -25,45 +25,51 @@ using namespace std;
 //some structures
 
 class Global {
-    public:
-	int xres, yres;
-	float w;
-	float vel; 
-	float pos[2];
+public:
+    int xres, yres;
+    float w;
+    float vel; 
+    float pos[2];
+    int bounceCount;
+    unsigned int boxColor[3];
 
-	Global() {
-	    xres = 400;
-	    yres = 200;
-	    w = 20.0f;
-	    vel = 30.0f;
-	    pos[0] = 0.0f + w;
-	    pos[1] = yres / 2.0f;
-	}
+    Global() {
+        xres = 400;
+        yres = 200;
+        w = 20.0f;
+        vel = 30.0f;
+        pos[0] = 0.0f + w;
+        pos[1] = yres / 2.0f;
+        bounceCount = 0;
+        boxColor[0] = 100; 
+        boxColor[1] = 120; 
+        boxColor[2] = 220; 
+    }
 } g;
 
 class X11_wrapper {
-    private:
-	Display *dpy;
-	Window win;
-	GLXContext glc;
-    public:
-	~X11_wrapper();
-	X11_wrapper();
-	void set_title();
-	bool getXPending();
-	XEvent getXNextEvent();
-	void swapBuffers();
-	void reshape_window(int width, int height);
-	void check_resize(XEvent *e);
-	void check_mouse(XEvent *e);
-	int check_keys(XEvent *e);
+private:
+    Display *dpy;
+    Window win;
+    GLXContext glc;
+public:
+    ~X11_wrapper();
+    X11_wrapper();
+    void set_title();
+    bool getXPending();
+    XEvent getXNextEvent();
+    void swapBuffers();
+    void reshape_window(int width, int height);
+    void check_resize(XEvent *e);
+    void check_mouse(XEvent *e);
+    int check_keys(XEvent *e);
 } x11;
 
 //Function prototypes
 void init_opengl(void);
 void physics(void);
 void render(void);
-
+void update_box_color();
 
 int main()
 {
@@ -71,17 +77,17 @@ int main()
     int done = 0;
     //main game loop
     while (!done) {
-	//look for external events such as keyboard, mouse.
-	while (x11.getXPending()) {
-	    XEvent e = x11.getXNextEvent();
-	    x11.check_resize(&e);
-	    x11.check_mouse(&e);
-	    done = x11.check_keys(&e);
-	}
-	physics();
-	render();
-	x11.swapBuffers();
-	usleep(200);
+        //look for external events such as keyboard, mouse.
+        while (x11.getXPending()) {
+            XEvent e = x11.getXNextEvent();
+            x11.check_resize(&e);
+            x11.check_mouse(&e);
+            done = x11.check_keys(&e);
+        }
+        physics();
+        render();
+        x11.swapBuffers();
+        usleep(200);
     }
     return 0;
 }
@@ -98,25 +104,25 @@ X11_wrapper::X11_wrapper()
     int w = g.xres, h = g.yres;
     dpy = XOpenDisplay(NULL);
     if (dpy == NULL) {
-	cout << "\n\tcannot connect to X server\n" << endl;
-	exit(EXIT_FAILURE);
+        cout << "\n\tcannot connect to X server\n" << endl;
+        exit(EXIT_FAILURE);
     }
     Window root = DefaultRootWindow(dpy);
     XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
     if (vi == NULL) {
-	cout << "\n\tno appropriate visual found\n" << endl;
-	exit(EXIT_FAILURE);
+        cout << "\n\tno appropriate visual found\n" << endl;
+        exit(EXIT_FAILURE);
     } 
     Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
     XSetWindowAttributes swa;
     swa.colormap = cmap;
     swa.event_mask =
-	ExposureMask | KeyPressMask | KeyReleaseMask |
-	ButtonPress | ButtonReleaseMask |
-	PointerMotionMask |
-	StructureNotifyMask | SubstructureNotifyMask;
+        ExposureMask | KeyPressMask | KeyReleaseMask |
+        ButtonPress | ButtonReleaseMask |
+        PointerMotionMask |
+        StructureNotifyMask | SubstructureNotifyMask;
     win = XCreateWindow(dpy, root, 0, 0, w, h, 0, vi->depth,
-	    InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
+            InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
     set_title();
     glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
     glXMakeCurrent(dpy, win, glc);
@@ -165,11 +171,11 @@ void X11_wrapper::check_resize(XEvent *e)
     //The ConfigureNotify is sent by the
     //server if the window is resized.
     if (e->type != ConfigureNotify)
-	return;
+        return;
     XConfigureEvent xce = e->xconfigure;
     if (xce.width != g.xres || xce.height != g.yres) {
-	//Window size did change.
-	reshape_window(xce.width, xce.height);
+        //Window size did change.
+        reshape_window(xce.width, xce.height);
     }
 }
 //-----------------------------------------------------------------------------
@@ -181,52 +187,52 @@ void X11_wrapper::check_mouse(XEvent *e)
 
     //Weed out non-mouse events
     if (e->type != ButtonRelease &&
-	    e->type != ButtonPress &&
-	    e->type != MotionNotify) {
-	//This is not a mouse event that we care about.
-	return;
+            e->type != ButtonPress &&
+            e->type != MotionNotify) {
+        //This is not a mouse event that we care about.
+        return;
     }
     //
     if (e->type == ButtonRelease) {
-	return;
+        return;
     }
     if (e->type == ButtonPress) {
-	if (e->xbutton.button==1) {
-	    //Left button was pressed.
-	    //int y = g.yres - e->xbutton.y;
-	    return;
-	}
-	if (e->xbutton.button==3) {
-	    //Right button was pressed.
-	    return;
-	}
+        if (e->xbutton.button==1) {
+            //Left button was pressed.
+            //int y = g.yres - e->xbutton.y;
+            return;
+        }
+        if (e->xbutton.button==3) {
+            //Right button was pressed.
+            return;
+        }
     }
     if (e->type == MotionNotify) {
-	//The mouse moved!
-	if (savex != e->xbutton.x || savey != e->xbutton.y) {
-	    savex = e->xbutton.x;
-	    savey = e->xbutton.y;
-	    //Code placed here will execute whenever the mouse moves.
+        //The mouse moved!
+        if (savex != e->xbutton.x || savey != e->xbutton.y) {
+            savex = e->xbutton.x;
+            savey = e->xbutton.y;
+            //Code placed here will execute whenever the mouse moves.
 
 
-	}
+        }
     }
 }
 
 int X11_wrapper::check_keys(XEvent *e)
 {
     if (e->type != KeyPress && e->type != KeyRelease)
-	return 0;
+        return 0;
     int key = XLookupKeysym(&e->xkey, 0);
     if (e->type == KeyPress) {
-	switch (key) {
-	    case XK_a:
-		//the 'a' key was pressed
-		break;
-	    case XK_Escape:
-		//Escape key was pressed
-		return 1;
-	}
+        switch (key) {
+            case XK_a:
+                //the 'a' key was pressed
+                break;
+            case XK_Escape:
+                //Escape key was pressed
+                return 1;
+        }
     }
     return 0;
 }
@@ -251,35 +257,36 @@ void init_opengl(void)
 
 void physics()
 {
-    //No physics yet.
+    // Update position
     g.pos[0] += g.vel;
+
+    // Check for collisions with the left and right walls
     if (g.pos[0] >= (g.xres - g.w)) {
-	g.pos[0] = (g.xres - g.w);
-	g.vel = -g.vel;
+        g.pos[0] = (g.xres - g.w);
+        g.vel = -g.vel;
+        g.bounceCount++;
+        update_box_color();
     }
     if (g.pos[0] <= g.w) {
-	g.pos[0] = g.w;
-	g.vel = -g.vel;
-    }		
+        g.pos[0] = g.w;
+        g.vel = -g.vel;
+        g.bounceCount++;
+        update_box_color();
+    }
 
-}       		
+    // If the window width is smaller than the box width, the box disappears
+    if (g.xres < g.w * 2) {
+        g.bounceCount = 0; // Reset the bounce count since the box will not bounce
+    }
+}       
 
-void render()
-{       		
-    glClear(GL_COLOR_BUFFER_BIT);
-    //draw the box
-    glPushMatrix();
-    glColor3ub(100, 120, 220);
-    glTranslatef(g.pos[0], g.pos[1], 0.0f);
-    glBegin(GL_QUADS);
-    glVertex2f(-g.w, -g.w);
-    glVertex2f(-g.w,  g.w);
-    glVertex2f( g.w,  g.w);
-    glVertex2f( g.w, -g.w);
-    glEnd();
-    glPopMatrix();
-}
+void update_box_color() {
+    int maxBounces = 100;
+    float t = (float)g.bounceCount / maxBounces;
+    t = t > 1.0f ? 1.0f : t;
 
+    g.boxColor[0] = (unsigned int)(100 + 155 * t);  
+    g.boxColor[1] = 120;                            
 
 
 
